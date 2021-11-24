186a23ce65aac515d230525349a202cb8818b415
CHAMA COMPUTERS <yohandissanayake13@gmail.com>
2021-11-24 15:49:25 +0530

    Design basic formula in Main.java file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,22 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        int mk1 = 45;
+        int mk2 = 55;
+        int mk3 = 66;
+        int total = mk1+mk2+mk3;
+        double avg = total/3;
+        String grd;
 
-Submission st1 = new Submission();
+        if (avg >= 50){
+            grd = "pass";
+        }else
+            grd = "fail";
+
+        System.out.println("Total "+total);
+        System.out.println("Average "+avg);
+        System.out.println("Grade "+grd);
+
+
     }
 }
